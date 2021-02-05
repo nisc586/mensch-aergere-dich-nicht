52ba47051c42f5cd5bb981aeea141efba7e94b10
Nils <nils.schohl@stud.uni-frankfurt.de>
2021-02-05 11:28:18 +0100

    Extract function for finding moves

--- src/main.py
+++ src/main.py
@@ -9,39 +9,39 @@ turn = 0
 MAX_FIELDS = 22
 DICE_MIN, DICE_MAX = 1, 6
 
+def find_move(pos, dice, occupied, max_fields):
+    """Returns new position if piece can reach it. None otherwise"""
+    new_pos = pos + dice
+    if new_pos > max_fields:
+        # piece can't move
+        return
+    elif new_pos in occupied:
+        # collision
+        return
+    else:
+        # can move
+        return new_pos
+
+
 while is_running:
     occupied = {pos1, pos2}
     if occupied == {MAX_FIELDS, MAX_FIELDS - 1}:
         print("Game over")
-        is_running = False
+        break
 
     dice = random.randint(DICE_MIN, DICE_MAX)
     print(f"{dice=}", end="\t")
 
-    new_pos1 = pos1 + dice
-    new_pos2 = pos2 + dice
-
     # Which moves are available?
     legal_moves = set()
 
-    if new_pos1 > MAX_FIELDS:
-        print("pice1 can't move")
-    else:
-        if new_pos1 in occupied:
-            print("collision")
-        else:
-            print("piece1 can reach", new_pos1)
-            legal_moves.add(("piece1", new_pos1))
-
-
-    if new_pos2 > MAX_FIELDS:
-        print("piece2 can't move")
-    else:
-        if new_pos2 in occupied:
-            print("collision")
-        else:
-            print("piece2 can reach", new_pos2)
-            legal_moves.add(("piece2", new_pos2))
+    new_pos1 = find_move(pos1, dice, occupied, MAX_FIELDS)
+    if new_pos1:
+        legal_moves.add(("piece1", new_pos1))
+    
+    new_pos2 = find_move(pos2, dice, occupied, MAX_FIELDS)
+    if new_pos2:
+        legal_moves.add(("piece2", new_pos2))
 
     print(f"{legal_moves=}")
 
@@ -50,8 +50,11 @@ while is_running:
         piece, n = random.choice(list(legal_moves))
         if piece == "piece1":
             pos1 = new_pos1
+            print(f"{piece} -> {new_pos1}", end="\t")
         elif piece == "piece2":
             pos2 = new_pos2
+            print(f"{piece} -> {new_pos2}", end="\t")
+
         else:
             raise AssertionError("unreachable")
     else:
