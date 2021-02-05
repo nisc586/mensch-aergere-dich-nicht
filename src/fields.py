31646708e22960df483edea14afd1d6576135173
Nils <nils.schohl@stud.uni-frankfurt.de>
2021-02-05 15:56:15 +0100

    Simplify classes for fields and pieces

--- src/fields.py
+++ src/fields.py
@@ -2,43 +2,16 @@ class Field():
     def __init__(self, val):
         self._value = val
         self._id = str(val)
-        self._piece = None
+        self.is_occupied = False
 
-    def get_piece(self):
-        return self._piece
-
-    def set_piece(self, p):
-        self._piece = p
-
-    def is_occupied(self):
-        return bool(self._piece)
 
     def __str__(self):
         return self._id
 
 
-class EndField(Field):
+class SpecialField(Field):
     def __init__(self, val, access):
-        assert access in {"r", "g", "b", "y"}
+        assert access in {"r", "g", "b", "y"}, "invalid color : {}".format(access)
         self.super().__init__(val)
         self.access = access
         self._id = f"{val}{access}"
-
-
-class StartField(Field):
-    def __init__(self, val, access):
-        self.super().__init__(val)
-        self.access = access
-        self._id = f"{val}{access}"
-        self._piece = []
-
-
-    def get_piece(self):
-        try:
-            return self._piece.pop()
-        except IndexError:
-            return None
-
-
-    def set_piece(self, p):
-        self._piece.append(p)
--- src/piece.py
+++ src/piece.py
@@ -1,15 +1,19 @@
 class Piece():
-    def __init__(self, color, home):
-        assert color in {"r", "g", "b", "y"}, f"invalid color {color}"
-        
+    def __init__(self, color, number, home, start):
+        assert color in {"r", "g", "b", "y"}, "invalid color {}".format(color)
         self._color = color
-        self.home = home
-        self.position = home
+        self._home = home
+        self._start = start
+        self._number = number
 
 
-    def move_home(self):
-        self.position = self.home
+    def get_home(self):
+        return self.home
 
 
     def get_color(self):
         return self._color
+
+
+    def __str__(self):
+        return "piece-{}{}".format(self._color, self._number)
