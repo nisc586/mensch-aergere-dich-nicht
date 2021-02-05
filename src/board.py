925e575d125a03224797debdee3c6052d7e68e85
Nils <nils.schohl@stud.uni-frankfurt.de>
2021-02-05 21:04:17 +0100

    Use classes in board and main, modify find_move

--- src/board.py
+++ src/board.py
@@ -1,54 +1,111 @@
+from piece import Piece
+from field import Field
+
 class Board():
-    def __init__(self):
-        self.MAX_FIELDS = 40
-        self.pieces = {"piece1": 1, "piece2": 0, "piece3": 0, "piece4": 0}
-    
+    def __init__(self, colors="g"):
+        assert set(colors) <= set("rgby")
+        self.PIECES_PER_PLAYER = 4
+        self.BOARD_LENGTH = 40
+        self.MAX_FIELDS = self.BOARD_LENGTH + self.PIECES_PER_PLAYER
 
-    def find_move(self, piece, pos, dice, dice_max):
-        """Returns new position if piece can reach it. None otherwise"""
-        # Special case: leave home base
-        if pos == 0:
-            if dice == dice_max:
-                new_pos = 1
-                if new_pos not in self.get_occupied():
-                    # Can leave home base
-                    return new_pos
-                else:
-                    # Field 1 occupied
-                    return
+        self.colors = colors
+        self.positions = {}
+        self.set_up()
+
+
+    def set_up(self):
+        """Creates pieces for each color and puts them on their starting fields"""
+        for col in self.colors:
+            home = Field(0, col)
+            if col == "g":
+                start = Field(1)
+                finish = Field(44, "g")
+            elif col == "r":
+                start = Field(11)
+                finish = Field(14, "r")
+            elif col == "b":
+                start = Field(21)
+                finish = Field(24, "b")
+            elif col == "y":
+                start = Field(31)
+                finish = Field(34, "y")
             else:
-                return
+                raise AssertionError("unreachable")
 
-        # Normal case: move forward
-        new_pos = pos + dice
-        if new_pos > self.MAX_FIELDS:
-            # piece can't move
+            for num in range(1, self.PIECES_PER_PLAYER + 1):
+                p = Piece(col, num, home, start, finish)
+                self.positions[p] = start if num==1 else home
+
+
+    def find_move(self, piece, dice, dice_max):
+        """Returns new position if piece can reach it. None otherwise"""
+        pos = self.positions[piece]
+
+        # Calculate next field value
+        new_value = pos.value + dice
+        distance_to_finish = piece.finish.value - new_value
+
+        if pos == piece.home:  # Special case: leave home base
+                new_field = piece.start if dice == dice_max else None
+
+        elif new_value > self.MAX_FIELDS:
             return
-        elif new_pos in self.get_occupied():
-            # collision
-            return
+
+        elif 0 <= distance_to_finish < self.PIECES_PER_PLAYER:  # Special case: right before finish
+            if (pos.value > piece.finish.value - self.PIECES_PER_PLAYER) and not pos.is_end_field:
+                return
+            else:
+                new_field = Field(new_value, piece.color)
+
+        else:  # Normal case
+            if pos.is_end_field:
+                return
+            else:
+                new_field = Field(new_value)
+
+        # Handle collisions
+        for p, f in self.positions.items():
+            if new_field == f:
+                if piece.color == p.color:
+                    # Collision with own color
+                    return
         else:
-            # can move
-            return new_pos
+            return new_field
 
 
     def collect_legal_moves(self, dice, dice_max):
         """Returns all possible moves for given pieces. May be empty list"""
         moves = list()
-        for piece, pos in self.pieces.items():
-            new_pos = self.find_move(piece, pos, dice, dice_max)
+        for piece, pos in self.positions.items():
+            new_pos = self.find_move(piece, dice, dice_max)
             if new_pos:
                 moves.append((piece, new_pos))
         return moves
 
 
     def move_piece(self, piece, new_pos):
-        self.pieces[piece] = new_pos
+        self.positions[piece] = new_pos
 
 
     def get_occupied(self):
-        return set(self.pieces.values())
+        return set(self.positions.values())
 
 
     def has_winner(self):
-        return self.get_occupied() >= {self.MAX_FIELDS-i for i in range(4)}
+        """Checks if one color occupies all end fields."""
+        for col in self.colors:
+            if col == "g":
+                start_val = 1
+            elif col == "r":
+                start_val = 11
+            elif col == "b":
+                start_val = 21
+            elif col == "y":
+                start_val = 31
+            else:
+                raise AssertionError("unreachable")
+            end_fields = {Field(start_val + i, col) for i in range(1, self.PIECES_PER_PLAYER+1)}
+            if end_fields <= self.get_occupied():
+                return True
+        else:
+            return False
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from board import Board
 import random
+import time
 
 DICE_MIN, DICE_MAX = 1, 6
 
@@ -14,12 +15,14 @@ while is_running:
         break
 
     dice = random.randint(DICE_MIN, DICE_MAX)
-    print(f"{dice=}", end="\t")
+    print(f"{dice=}")
 
     # Which moves are available?
     legal_moves = board.collect_legal_moves(dice, DICE_MAX)
 
-    print(f"{legal_moves=}")
+    for piece, new_position in legal_moves:
+        print(piece, "->", new_position)
+    print()
 
     # Pick a random move
     if legal_moves:
@@ -30,6 +33,7 @@ while is_running:
         print("No moves available")
 
     turn += 1
+    time.sleep(1)
     print()
 
 print(f"{turn=}")
