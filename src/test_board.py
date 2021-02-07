8d442225994911f84b6952ad72523584ec9d2fcc
Nils <nils.schohl@stud.uni-frankfurt.de>
2021-02-07 19:36:06 +0100

    Update tests

--- src/test_board.py
+++ src/test_board.py
@@ -17,24 +17,38 @@ PIECES = {
 def test_suggest_moves_normal():
     board = Board("g")
     piece = PIECES["green1"]
-    suggested_move = board.suggest_moves(piece, 3)
-    assert [Field("4")] == suggested_move
+    assert [(Field("4"), "normal move")] == board.suggest_moves(piece, 3)
 
 
 def test_leave_base_normal():
     board = Board("gb")
     piece = PIECES["green1"]
     board.move_home(piece)
-    suggested_move = board.suggest_moves(piece, 6)
-    assert [Field("1")] == suggested_move
+    assert [(Field("1"), "leave base")] == board.suggest_moves(piece, 6)
 
 
 def test_no_suggest_end_field_other_color():
     board = Board("rgby")
     piece = PIECES["green1"]
     board.move_piece(piece, Field("19"))
+    assert [(Field("23"), "normal move")] == board.suggest_moves(piece, 4)
+
+
+def test_reach_end_field():
+    board = Board("rgby")
+    piece = PIECES["green1"]
+    board.move_piece(piece, Field("40"))
     suggested_moves = board.suggest_moves(piece, 4)
-    assert [Field("23")] == suggested_moves
+    assert 2 == len(suggested_moves)
+    assert (Field("B4", color="g"), "reach end field") in suggested_moves 
+
+
+def test_beat_peace():
+    board = Board("rgby")
+    green = PIECES["green1"]
+    blue = PIECES["blue1"]
+    board.move_piece(blue, Field("4"))
+    assert [(Field("4"), "beat piece-b1")] == board.suggest_moves(green, 3)
 
 
 def test_end_field_blocked():
